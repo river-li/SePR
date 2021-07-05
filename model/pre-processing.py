89cfd0563c014ab0fe823e9ef23a4846ee38345c
River-Li <lizic0228@foxmail.com>
2021-07-05 23:21:26 +0800

    fixed an error in adding lines to adds array and decs array

--- model/pre-processing.py
+++ model/pre-processing.py
@@ -35,11 +35,19 @@ def parse_commit(commit_path):
         commit_size = len(cnt)
 
         diff = cnt[last:commit_size]
-        lines = diff.split('\n')
+        lines = diff.rstrip().split('\n')
         adds = []
         decs = []
 
-        for line in lines:
+        # The first four lines look like:
+        # 
+        # diff --git a/fs/stat.c b/fs/stat.c
+        # index 29c5fe4f8b6..d712a0dfb50f 100644
+        # --- a/fs/stat.c
+        # +++ b/fs/stat.c
+        # 
+        # we just start from the fifth line
+        for line in lines[4:]:
             if line[0]=='+':
                 adds.append(line)
             elif line[0]=='-':
