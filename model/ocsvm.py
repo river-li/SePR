db59244ac55e27fb4804f835f5b7116d9d9abec5
River-Li <lizic0228@foxmail.com>
2021-07-06 20:25:11 +0800

    finished a model using One-Class SVM

--- model/ocsvm.py
+++ model/ocsvm.py
@@ -1,4 +1,18 @@
+import pandas as pd
 import numpy as np
 from sklearn import svm
 
-clf = svm.OneClassSVM(nu=0.1,kernel='rbf',gamma=0.1)
+
+class OCSVM:
+    def __init__(self, csv):
+        self.data = np.array(pd.read_csv(csv)).T[1:].T
+        # when we save as csv, the
+        self.model = svm.OneClassSVM(nu=0.1,kernel='sigmoid',gamma=0.1)
+        self.model.fit(self.data)
+
+    def train(self,data):
+        self.model.fit(data)
+
+    def predict(self,data):
+        predict = self.model.predict(data)
+        return predict
