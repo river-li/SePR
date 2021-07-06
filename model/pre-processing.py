87138907337be78f6e8f995b84cb186b2a503dce
River-Li <lizic0228@foxmail.com>
2021-07-06 21:17:11 +0800

    separate parse_patch and parse_commit, in order to support externel calls

--- model/pre-processing.py
+++ model/pre-processing.py
@@ -199,53 +199,58 @@ def parse_commit(commit_path):
     log('[+] Started parsing ' + commit_path, 'CYAN')
     with open(os.path.join(commit_path, 'patch'), 'r') as pf:
         cnt = pf.read()
-        # diff_str = 'diff --git a(/[a-zA-Z0-9_]+)+\.c b(/[a-zA-Z0-9_]+)+\.c'
-        # diff_pattern = re.compile(diff_str)
-
-        diff_str = 'diff --git'
-        separator = [cnt.index(diff_str)]
-        # separator is where "diff --git xxxxx xxxx" separate each file log
-
-        sep = 0
-        flag = True
-        while flag:
-            if cnt.index(diff_str, sep) != cnt.rindex(diff_str):
-                sep = cnt.index(diff_str, sep + 1)
-                separator.append(sep)
-            else:
-                if cnt.rindex(diff_str) in separator:
-                    flag = False
-                else:
-                    separator.append(cnt.rindex(diff_str))
-
-        changed_files = len(separator)
-        last = separator.pop()
-        commit_size = len(cnt)
-
-        diff = cnt[last:commit_size]
-        lines = diff.rstrip().split('\n')
-        adds = []
-        decs = []
-
-        # The first four lines look like:
-        # 
-        # diff --git a/fs/stat.c b/fs/stat.c
-        # index 29c5fe4f8b6..d712a0dfb50f 100644
-        # --- a/fs/stat.c
-        # +++ b/fs/stat.c
-        # 
-        # we just start from the fifth line
-        for line in lines[4:]:
-            if line[0] == '+':
-                adds.append(line[1:])
-            elif line[0] == '-':
-                decs.append(line[1:])
-
-        vec = extract_features(adds, decs)
-        vec[0] = changed_files
-
-        success('extract vector from ' + commit_path + ' done!')
+        vec = parse_patch(cnt)
         return vec
+
+
+def parse_patch(cnt):
+    # diff_str = 'diff --git a(/[a-zA-Z0-9_]+)+\.c b(/[a-zA-Z0-9_]+)+\.c'
+    # diff_pattern = re.compile(diff_str)
+
+    diff_str = 'diff --git'
+    separator = [cnt.index(diff_str)]
+    # separator is where "diff --git xxxxx xxxx" separate each file log
+
+    sep = 0
+    flag = True
+    while flag:
+        if cnt.index(diff_str, sep) != cnt.rindex(diff_str):
+            sep = cnt.index(diff_str, sep + 1)
+            separator.append(sep)
+        else:
+            if cnt.rindex(diff_str) in separator:
+                flag = False
+            else:
+                separator.append(cnt.rindex(diff_str))
+
+    changed_files = len(separator)
+    last = separator.pop()
+    commit_size = len(cnt)
+
+    diff = cnt[last:commit_size]
+    lines = diff.rstrip().split('\n')
+    adds = []
+    decs = []
+
+    # The first four lines look like:
+    #
+    # diff --git a/fs/stat.c b/fs/stat.c
+    # index 29c5fe4f8b6..d712a0dfb50f 100644
+    # --- a/fs/stat.c
+    # +++ b/fs/stat.c
+    #
+    # we just start from the fifth line
+    for line in lines[4:]:
+        if line[0] == '+':
+            adds.append(line[1:])
+        elif line[0] == '-':
+            decs.append(line[1:])
+
+    vec = extract_features(adds, decs)
+    vec[0] = changed_files
+
+    success('Extract vector from done!')
+    return vec
 
 
 if __name__ == '__main__':
